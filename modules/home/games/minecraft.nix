476df9278366994b227188f0f4d9f03c5d0473d5
nopoxxx <dmitriy.nopox@gmail.com>
2026-05-16 22:13:58 +0300

    pohyi na prism

--- modules/home/games/minecraft.nix
+++ modules/home/games/minecraft.nix
@@ -1,18 +1,5 @@
 { pkgs, ... }: {
   programs.prismlauncher = {
     enable = true;
-
-    extraPackages = [
-      (pkgs.runCommand "prismlauncher-gruvbox-soft" {} ''
-        mkdir -p $out/share/PrismLauncher/themes
-        cp -r ${pkgs.fetchFromGitHub {
-          owner = "PrismLauncher";
-          repo = "Themes";
-          rev = "main";
-          sha256 = "sha256-aaphBE1QkJid9IAUZZB3zcabqSAd/GoMsl9seulv8Tw=";
-        }}/themes/Gruvbox-Soft \
-          $out/share/PrismLauncher/themes/
-      '')
-    ];
   };
 }
