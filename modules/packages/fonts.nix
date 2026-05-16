37f303f179e73f76f6cba914a564a7415d79bc16
nopoxxx <dmitriy.nopox@gmail.com>
2026-05-17 00:37:14 +0300

    add fonts

--- modules/packages/fonts.nix
+++ modules/packages/fonts.nix
@@ -3,5 +3,10 @@
 
   fonts.packages = with pkgs; [
     (nerd-fonts.jetbrains-mono)
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+      fira-code
+      fira-code-symbols
   ];
 }
